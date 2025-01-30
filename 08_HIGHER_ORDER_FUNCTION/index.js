3104c2708db8531c7d2a3274bd48e16c19c79cf3
Gourav <gourav.chouhan1729@gmail.com>
2025-01-30 17:36:58 +0530

    30-01-2025

--- 08_HIGHER_ORDER_FUNCTION/index.js
+++ 08_HIGHER_ORDER_FUNCTION/index.js
@@ -59,12 +59,12 @@ let products = [
 // ["Bob", "Charlie"]
 
 
-// let adultNames = users
-//   .filter(user => user.age >= 18)
+let adultNames = users
+  .filter(user => user.age >= 18).map(user => user.name)
 
 
  
-// console.log(adultNames[0].name);
+console.log(adultNames);
 
 
 // 1. Use `map` to double the numbers in an array: `[1, 2, 3, 4]`.
@@ -79,7 +79,68 @@ let products = [
 //    ```
 // 4. Extract all even numbers greater than 10, square them, and return the new array from `[5, 12, 18, 7, 20]`.
 
-// 5. 20, 10, 5, 15, 20, 14;
+// 5. 20, 10, 5, 15, 20, 14;  
+
+// let stock = [7,6,4,3,1];
+
+
+//reduce :
+
+let arr = [1, 2, 3, 4, 5];
+
+
+
+// 0 + 1 = 1 
+// 1 + 2 = 3
+// 3 + 3 = 6
+// 6 + 4 = 10
+// 10 + 5 = 15
+
+
+let sum = arr.reduce((acc, curr) => acc + curr, 0 )
+
+
+console.log(sum)
+
+
+
+//spread operator
+
+let arr10 = [1, 2, 3];
+let arr20 = [4, 5, 6];
+
+let arr30 = [...arr10, ...arr20]
+console.log(arr30)
+
+let colors = ['red', 'green', 'blue'];
+let [first, second] = colors;
+console.log(first); // red
+console.log(second); // green
+
+let user = { name: 'John', age: 30 };
+let { name, age } = user;
+console.log(name); // John
+console.log(age); // 30
+
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+
+// ## **11. Assignment Questions**
+// 1. Write a function that uses `reduce()` to calculate the product of an array.
+// 2. Use destructuring to swap the values of two variables.
+// 3. Write a program to merge two arrays using the spread operator.
+
+// 4)On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+// Find and return the maximum profit you can achieve.
+
+
+
+
+
+
+
 
 
 
