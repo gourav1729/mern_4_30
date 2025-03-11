e5bbdf0068526c7326310a9add2bc0720829158a
Gourav <gourav.chouhan1729@gmail.com>
2025-03-11 17:42:56 +0530

    11-03-2023

--- 12_REACT_JS/todolist/src/Todolist.jsx
+++ 12_REACT_JS/todolist/src/Todolist.jsx
@@ -1,39 +1,71 @@
-import React from 'react'
-import './Todolist.css'
-import { useState } from 'react'
-import { v4 as uuidv4} from "uuid"
+import React from "react";
+import "./Todolist.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function Todolist() {
+  let [todos, setTodos] = useState(["Sample task"]); 
 
-    let [todos, setTodos] = useState([{task: "sample task"}])
-    let [newTodo, setnewTodos] = useState("") 
+  let [newTodo, setNewTodo] = useState("");
 
-    
-    // npm -> commandline jo ki packages install karne me help karta hai. And secondly library, for installing packages.
+  let addNewTask = () => {
+    // console.log('ADD NEW TASK')
+    setTodos([...todos, newTodo]);  
+    setNewTodo("")
+    console.log(todos)
 
-    console.log(uuidv4())
-    // 6bee10d1-27b8-4b9d-9c84-eb121de8f0ea
-    // f45bc6b3-852a-45b3-a71c-0c486038979e
-    
+  };
 
-    let addNewTask = () => {
-        // console.log('ADD NEW TASK')
 
-    }
+  let updateTodoValue = (event) => {
+   
 
+    setNewTodo(event.target.value);
+  };
 
   return (
     <>
       <h1>Todolist</h1>
 
+      <input
+        placeholder="add new todos"
+        value={newTodo}
+        onChange={updateTodoValue}
+      />
 
       {/*1) CREATE A NEW TODO */}
       <button onClick={addNewTask}>ADD TASK</button>
 
+      <h2>TASK TO DO </h2>
+      <ul>
+          {
+            todos.map((todo) => (
 
+              <li>{todo}</li>
+
+            ))} 
+           
+      </ul>
 
     </>
-  )
+  );
 }
 
-export default Todolist
+export default Todolist;
+
+// 1)add    :
+// 2)delete :
+
+// 1) list
+// 2) ntask 1
+// 3) task 2
+// 4) task 2
+// 5) task 1
+
+//
+
+// console.log(uuidv4())
+// 6bee10d1-27b8-4b9d-9c84-eb121de8f0ea
+// f45bc6b3-852a-45b3-a71c-0c486038979e
+
+// npm -> commandline jo ki packages install karne me help karta hai. And secondly library, for installing packages.
